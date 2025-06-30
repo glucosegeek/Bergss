94e00f50bc9ddfdf145d06b7144d4b979115f5c6
glucosegeek <glucosegeek@proton.me>
2025-06-30 20:33:51 +0200

    Updated HeroHeadline.tsx

--- src/components/Hero/HeroHeadline.tsx
+++ src/components/Hero/HeroHeadline.tsx
@@ -36,7 +36,7 @@ const HeroHeadline: React.FC = () => {
       </span>
       <br />
       <span className="inline-block mb-2">
-        o 25% dzięki
+        o 26% dzięki
       </span>
       <br />
       <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-purple-500">
