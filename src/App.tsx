29247d33254060564be5a91ee7864b022ebd09eb
glucosegeek <glucosegeek@proton.me>
2025-06-30 20:55:31 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
        {/*  <FloatingNotification
           title="Witaj!"
           message="Odkryj, jak AI może zrewolucjonizować Twoją sprzedaż. Napisz do nas i omówimy razem jak możemy Ci pomóc."
-          duration={7000} */}
+          duration={7000} }
         />
         <Routes>
           <Route path="/" element={
