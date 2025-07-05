e4a2996aaa8d37fbe0eaa338af901f4a27a8dc77
glucosegeek <glucosegeek@proton.me>
2025-07-05 19:24:42 +0200

    Updated WhoIsThisFor.tsx

--- src/components/WhoIsThisFor/WhoIsThisFor.tsx
+++ src/components/WhoIsThisFor/WhoIsThisFor.tsx
@@ -6,8 +6,7 @@ const WhoIsThisFor: React.FC = () => {
   const clientTypes = [
     {
       icon: <Building2 className="w-5 h-5 sm:w-6 sm:h-6" />,
-      type: "Agencje marketingowe",
-      description: "Skaluj usługi dla klientów dzięki automatyzacji opartej na AI"
+      type: "Agencje marketingowe"
     },
     {
       icon: <Rocket className="w-5 h-5 sm:w-6 sm:h-6" />,
